e7f7f49a88a3aee4bbcecf6bd39a1cbb5b33240a
Norbert <69053304+norbi999@users.noreply.github.com>
2022-06-09 12:37:49 +0200

    complete config class

---
+++ src/main/java/com/junioroffers/infrastructure/offer/error/RestTemplateResponseErrorHandler.java
@@ -0,0 +1,6 @@
+package com.junioroffers.infrastructure.offer.error;
+
+import org.springframework.web.client.DefaultResponseErrorHandler;
+
+public class RestTemplateResponseErrorHandler extends DefaultResponseErrorHandler {
+}
