e4683ec5089b3c3213c37b99399b6a7c93695532
Norbert <69053304+norbi999@users.noreply.github.com>
2022-07-02 11:52:55 +0200

    docker/mongo db first step

--- src/main/java/com/junioroffers/offer/domain/OfferService.java
+++ src/main/java/com/junioroffers/offer/domain/OfferService.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class OfferService {
 
     public List<OfferDto> findAllOffers(){
-        final OfferDto cybersourceDto= OfferMapper.mapToOfferDto(UUID.fromString("niewiem"),
+        final OfferDto cybersourceDto= OfferMapper.mapToOfferDto(UUID.fromString("7b3e02b3-6b1a-4e75-bdad-cef5b279b074"),
                 "Software Engineer - Mobile (m/f/d)",
                 "4k - 8k PLN",
                 "https://nofluffjobs.com/pl/job/software-engineer-mobile-m-f-d-cybersource-poznan-entavdpn",
